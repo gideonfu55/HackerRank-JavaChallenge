8fc6e3ff232d28c4ef5930cc32f99a92db3f51d8
jcmonkey87 <fuzhonglong@gmail.com>
2023-03-19 17:20:57 +0800

    spacing

--- DataTypes.java
+++ DataTypes.java
@@ -43,10 +43,12 @@ import java.util.Scanner;
 
 public class DataTypes {
     public static void main(String[] args) {
+
     Scanner scan = new Scanner(System.in);
     int t = scan.nextInt();
 
     for(int i = 0; i < t; i++) {
+      
       try {
         long x = scan.nextLong();
         System.out.println(x + " can be fitted in:");
