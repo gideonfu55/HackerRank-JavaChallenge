54fa3e6c962085e17ae0fadd6fec71dae7e36007
jcmonkey87 <fuzhonglong@gmail.com>
2023-03-19 17:13:41 +0800

    update indent

--- DataTypes.java
+++ DataTypes.java
@@ -48,16 +48,16 @@ public class DataTypes {
 
     for(int i = 0; i < t; i++) {
       try {
-          long x = scan.nextLong();
-          System.out.println(x + " can be fitted in:");
-          if(x >=- 128 && x <= 127) System.out.println("* byte");
-          //Complete the code
-          if(x >= -32768 && x <= 32767) System.out.println("* short");
-          if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
-          if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
+        long x = scan.nextLong();
+        System.out.println(x + " can be fitted in:");
+        if(x >=- 128 && x <= 127) System.out.println("* byte");
+        if(x >= -32768 && x <= 32767) System.out.println("* short");
+        if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
+        if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
       }
+      
       catch(Exception e) {
-          System.out.println(scan.next()+" can't be fitted anywhere.");
+        System.out.println(scan.next()+" can't be fitted anywhere.");
       }
     }
 
