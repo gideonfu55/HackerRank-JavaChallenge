296635d3e44a5ff5d7a16ddd2d9968f5d3785f3c
jcmonkey87 <fuzhonglong@gmail.com>
2023-03-26 19:29:34 +0800

    formatting update

--- DateAndTime.java
+++ DateAndTime.java
@@ -8,21 +8,30 @@ public class DateAndTime {
   // https://www.hackerrank.com/challenges/java-date-and-time/problem?isFullScreen=true
 
   class Result {
+
     public static String findDay(int month, int day, int year) {
+
       Calendar cal = Calendar.getInstance();
+
       cal.set(Calendar.MONTH, month - 1);
       cal.set(Calendar.DAY_OF_MONTH, day);
       cal.set(Calendar.YEAR, year);
+
       String dayOfWeek = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+      
       return dayOfWeek;
+
     }
+
   }
 
   class Result2 {
 
     public static String findDay(int month, int day, int year) {
+
       LocalDate date = LocalDate.of(year, month, day);
       return date.getDayOfWeek().name();
+
     }
     
   }
@@ -33,4 +42,5 @@ public class DateAndTime {
 
     System.out.println(res);
   }
+
 }
